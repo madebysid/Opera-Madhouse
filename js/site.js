b649f03bbc84e344bc22c70f371f02e96121df8c
Siddhant Sinha <llsiddhantll@gmail.com>
2014-11-08 19:43:36 +0530

    v0.0
    Fresh Start. Testing Canvas element.

---
+++ js/site.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	var canvas = document.getElementById('mainGame');
+	var ctx = canvas.getContext('2d');
+
+	ctx.fillStyle = "rgb(200,0,0)";
+	ctx.fillRect(100,100,100,100);
+});
