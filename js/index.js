1a5d1fa6c7947e2a47a5db96796e47943a8d0a0f
Siddhant Sinha <llsiddhantll@gmail.com>
2014-11-09 23:04:27 +0530

    v0.2
    > Added obstacle class
    > Obstacles are now created as objects of 'veg' class
    > Multiple obstacles possible
    
    Bugs:
    > Collision is erratic
    
    To do:
    > Splash screen
    > Retry Screen
    > Graphics

--- js/index.js
+++ js/index.js
@@ -1,49 +1,40 @@
 $(document).ready(function(){
+    gameInit();
+});
 
-    var devHeight = window.innerHeight,
-        devWidth  = window.innerWidth;
-    
-    //SINGER
+var devHeight = parseInt(window.innerHeight),
+    devWidth  = parseInt(window.innerWidth);
 
-    var singerPosX = 0,
-        singerPosY = 0,
-        singerWidth = 50,
-        singerHeight = 50;
+var singerPosX = 0,
+    singerPosY = 0,
+    singerWidth = devWidth/6,
+    singerHeight = devHeight/15;
 
-    //VEGGIES
+var vegCount = 0;
 
-    var vegWidth = 40;
-        vegHeight = 40;
-        vegLevel = 1;
-        vegSpeed = Math.random()*vegLevel*100,
-        vegPosX = Math.random()*devWidth + vegWidth,
-        vegPosY = devHeight;
+var veg = function(vegId,vegLevel){
+    var vegWidth = devWidth/12;
+    var vegHeight = vegWidth;
+    var vegLevel = vegLevel;
+    var vegSpeed = Math.random()*vegLevel*100;
+    var vegPosX = Math.random()*devWidth + vegWidth;
+    var vegPosY = devHeight - vegHeight;
 
-    var vegCreate = function()
-    {
-        vegSpeed = Math.random()*vegLevel*100,
-        vegPosX = Math.random()*devWidth - vegWidth,
-        vegPosY = devHeight;
-    },
+    console.log("veg"+vegId+"created");
 
-    moveVeg = function(){
+    var moveVeg = function(){
         vegPosY = vegPosY - vegSpeed;
         if(vegPosY < 0){
-            vegCreate();
+            stopVeg();
         }
-        $(".veg").css("left" , vegPosX + 'px');
-        $(".veg")
-            .css("transition-property","translate 1s")
-            .css("transition-duration","0.2s")
-            .css("transition-timing-function","linear")
+        $("#veg"+vegId).css("left" , vegPosX + 'px');
+        $("#veg"+vegId)
+            .css("transition-property" , "translate 1s")
+            .css("transition-duration" , "0.2s")
+            .css("transition-timing-function" , "linear")
             .css("transform" , "translate(0px," + vegPosY + "px)");
-    },
-    
-    levelUp = function(){
-        vegLevel = vegLevel + 1;
-    },
-
-    collideCheck = function(){
+    };
+    var collideCheck = function(){
         if(vegPosX > singerPosX - vegWidth && vegPosX < singerPosX + singerWidth)
         {
             if(vegPosY < singerPosY && vegPosY > singerPosY - singerHeight)
@@ -51,60 +42,52 @@ $(document).ready(function(){
                 gameOver();
             }
         }
-    },
+    };
+    var stopVeg = function(){
+        clearInterval(moveThatAss);   
+    };
+    var levelUp = function(){
+        vegLevel = vegLevel + 1;
+    };
 
-    gameOver = function(){
-        clearInterval(moveThatAss);
-        alert("Game Over");
-    }
-
-    var moveThatAss = window.setInterval(function(){
+    var moveThatAss = setInterval(function(){
         moveVeg();
         collideCheck();
     },200);
 
+};
+
+var vegCreate = function(){
+    $(".mainGame")
+        .append("<div class='veg' id='veg'"+ vegCount +">VEG "+ vegCount + "</div>");
+    
+    newVeg = new veg(0,1);
+    newVeg = new veg(1,1);
+    newVeg = new veg(2,1);
+    newVeg = new veg(3,1);
+    newVeg = new veg(4,1);
+    newVeg = new veg(5,1);
+    newVeg = new veg(6,1);
+    newVeg = new veg(7,1);
+    newVeg = new veg(8,1);
+    newVeg = new veg(9,1);
+};
+
+var gameInit = function(){
+    $('#divDrag')
+        .css("width" , singerWidth + 'px')
+        .css("height" , singerHeight + 'px');
+
+    vegCreate();
+
     // window.setInterval(function(){
     //     levelUp();
     // },5000);
+},
 
-    interact('.draggable')
-        .draggable({
-            onmove: function (event) {
-                var target = event.target,
-                    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-                    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-                target.style.webkitTransform =
-                target.style.transform =
-                    'translate(' + x + 'px, ' + y + 'px)';
-
-                target.setAttribute('data-x', x);
-                target.setAttribute('data-y', y);
-                singerPosX = x;
-                singerPosY = y;
-            },
-            // onend: function (event) {
-            //     var textEl = event.target.querySelector('p');
-                
-            //     textEl && (textEl.textContent =
-            //         'moved a distance of '
-            //         + (Math.sqrt(event.dx * event.dx +
-            //                      event.dy * event.dy)|0) + 'px');
-            // }
-        })
-        .inertia(true)
-        .restrict({
-            drag: "parent",
-            endOnly: true,
-            elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-        });
-
-    //DOC READY ENDS
-    });
-
-
-
-
+    gameOver = function(){
+        alert("Game Over, Bitch");
+    };
 
 var app = {
     // Application Constructor
@@ -139,3 +122,27 @@ var app = {
 };
 
 app.initialize();
+
+interact('.draggable')
+    .draggable({
+        onmove: function (event) {
+            var target = event.target,
+                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+            target.style.webkitTransform =
+            target.style.transform =
+                'translate(' + x + 'px, ' + y + 'px)';
+
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+            singerPosX = x;
+            singerPosY = y;
+        }
+    })
+    .inertia(true)
+    .restrict({
+        drag: "parent",
+        endOnly: true,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    });
